11c6ccd165f294e3aca35556144ac56ba3c8b09b
nikolaihg <nikolai.gangstoe@gmail.com>
2024-12-04 19:15:13 +0100

    day1 part2

--- day01/day01.py
+++ day01/day01.py
@@ -1,7 +1,6 @@
 filePath = "C:\\Users\\nikol\\Desktop\\aoc24\\day01\\data.txt"
 
 def part1(fp):
-    distance = 0
     l1 = []
     l2 = []
 
@@ -10,8 +9,23 @@ def part1(fp):
             pair = (line.strip()).split()
             l1.append(int(pair[0]))
             l2.append(int(pair[1]))
-        distance = sum([abs(x - y) for x, y in zip(sorted(l1),sorted(l2))])
-        
+    
+    distance = sum([abs(x - y) for x, y in zip(sorted(l1),sorted(l2))])
+
     return distance
 
-print(part1(filePath))
+def part2(fp):
+    l1 = []
+    l2 = []
+
+    with open(fp, "r") as file:
+        for line in file.readlines():
+            pair = (line.strip()).split()
+            l1.append(int(pair[0]))
+            l2.append(int(pair[1]))
+
+    similarityScore = (sum([(l1.count(x) * x) for x in l2]))
+    return similarityScore
+
+print(f"part 1 answer: {part1(filePath)}")
+print(f"part 2 answer: {part2(filePath)}")
